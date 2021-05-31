21a283ae43ee13589a0f4177f72409d85c196cb0
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-05-31 15:29:38 -0300

    Cria componente footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
