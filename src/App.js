f1ee44bea6dfbb75c76b6baae44732ee9b00823d
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-05-31 15:30:41 -0300

    Cria rotas de home e login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
+
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>App Criado</h1>
-      </div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
