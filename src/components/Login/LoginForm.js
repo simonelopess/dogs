898ed210899773386318d39849b35d1f476adc83
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-05-31 22:09:57 -0300

    Lista usuário logado

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
-import { TOKEN_POST } from "../../api";
+import { TOKEN_POST, USER_GET } from "../../api";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
+  /* Valida se token já existe */
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  /* GET USER  */
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  /* POST USER */
   async function handleSubmit(event) {
     event.preventDefault();
 
@@ -20,7 +37,9 @@ const LoginForm = () => {
 
       const response = await fetch(url, options);
       const json = await response.json();
+      window.localStorage.setItem("token", json.token);
       console.log(json);
+      getUser(json.token);
     }
   }
 
