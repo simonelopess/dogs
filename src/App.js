4f4dc7646e53791a5aef7705adaef9f7a5b626da
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-06-02 22:06:25 -0300

    Cria componente User

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <Route path="/conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ src/components/User/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+  return (
+    <div>
+      <h1>Usuário</h1>
+    </div>
+  );
+};
+
+export default User;
