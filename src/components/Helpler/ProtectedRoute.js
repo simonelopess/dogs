92cff5036d194db966aafda16fa2d9e9cf75ebf3
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-06-02 22:14:43 -0300

    Proteção de rota

---
+++ src/components/Helpler/ProtectedRoute.js
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { Navigate, Route } from "react-router";
+import { UserContext } from "../../UserContext";
+
+const ProtectedRoute = (props) => {
+  const { login } = useContext(UserContext);
+  if (login === true) return <Route {...props} />;
+  else if (login === false) return <Navigate to="/login" />;
+  else return null;
+};
+
+export default ProtectedRoute;
