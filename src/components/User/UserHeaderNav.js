82f35ac2f8a6cf7dba0e4f361e9a15e5b08becfd
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-06-08 21:11:06 -0300

    Menu mobile

--- src/components/User/UserHeaderNav.js
+++ src/components/User/UserHeaderNav.js
@@ -1,37 +1,60 @@
-import React, { useContext, useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
 import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
 import { ReactComponent as AdicionarFoto } from "../../Assets/adicionar.svg";
 import { ReactComponent as Sair } from "../../Assets/sair.svg";
 import styles from "./UserHeaderNav.module.css";
+import useMedia from "../../Hooks/useMedia";
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = useState(null);
+  const mobile = useMedia("(max-width: 40rem");
   const { userLogout } = useContext(UserContext);
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const { pathname } = useLocation();
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta" end activeClassName={styles.active}>
-        <MinhasFotos /> {mobile && "Minhas Fotos"}
-      </NavLink>
-      <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
-        <Estatisticas />
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
 
-        {mobile && "Estastíticas"}
-      </NavLink>
-      <NavLink to="/conta/postar" activeClassName={styles.active}>
-        <AdicionarFoto />
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/conta" end activeClassName={styles.active}>
+          <MinhasFotos /> {mobile && "Minhas Fotos"}
+        </NavLink>
+        <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
+          <Estatisticas />
 
-        {mobile && "Adicionar Foto"}
-      </NavLink>
-      <button onClick={userLogout}>
-        <Sair />
+          {mobile && "Estastíticas"}
+        </NavLink>
+        <NavLink to="/conta/postar" activeClassName={styles.active}>
+          <AdicionarFoto />
 
-        {mobile && "Sair"}
-      </button>
-    </nav>
+          {mobile && "Adicionar Foto"}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Sair />
+
+          {mobile && "Sair"}
+        </button>
+      </nav>
+    </>
   );
 };
 
