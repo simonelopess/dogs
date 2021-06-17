3b4424ac3f149c3506db99775c8ba4e2eec327b0
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-06-17 17:04:00 -0300

    Criação ocmponente Feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,12 @@
 import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
-  return <div></div>;
+  return <div>
+    <FeedModal />
+    <FeedPhotos />
+  </div>;
 };
 
 export default Feed;
---
+++ src/components/FeedModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FeedModal = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default FeedModal;
